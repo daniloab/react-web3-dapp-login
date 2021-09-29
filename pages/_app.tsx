69e722ea645dcd7c93051cdf59402b9908b1a300
Danilo <daniloassis.ti@gmail.com>
2021-09-28 23:44:40 -0300

    feat(init): ༼ つ ◕_◕ ༽つ react-web3-dapp-login

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { DAppProvider } from "@usedapp/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DAppProvider config={{}}>
+      <Component {...pageProps} />
+    </DAppProvider>
+  );
 }
-export default MyApp
+
+export default MyApp;
